6912a9f3520f91d3b2475ef2b3f78cc2df766480
venkatasaikumar <venkat6090683@gmail.com>
2023-09-20 11:27:37 +0530

    first commit

---
+++ sample_2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author venkat
+ *
+ */
+module sample_2 {
+}
---
+++ sample_2/src/sample_package/sample_class.java
@@ -0,0 +1,7 @@
+package sample_package;
+
+public class sample_class {
+public static void main(String[] args) {
+	System.out.println("sample class");
+}
+}
